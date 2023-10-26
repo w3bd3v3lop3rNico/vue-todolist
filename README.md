7170ddf1964053f4627dd80e5e8c92c3f32d3eb4
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-26 16:16:45 +0200

    Fixed doneitem and additem js

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Rifare l'esercizio della to do list.
 Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 - text, una stringa che indica il testo del todo
 - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-MILESTONE 1
+MILESTONE 1 FATTO
 Stampare all'interno di una lista HTML un item per ogni todo.
 Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-MILESTONE 2
+MILESTONE 2 FATTO
 Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const { createApp } = Vue;
 createApp ({
     data() {
         return {
-            todo: 0,
+            newTodo: {
+                text: '',
+                done: false,
+            },
+            
             todos: [
                 {
                     text: 'Fare i compiti',
@@ -21,11 +25,33 @@ createApp ({
         }
     },
     methods: {
-        deleteItem (itemIndex) {
+        addItem () {
+            const newTodo = {
+                text: this.newTodo.text,
+                done: false,
+            }
+            if (newTodo.text !== '') {
+                this.todos.unshift(newTodo);
+            }
+            else {
+                this.newTodo.text = '';
+            }
+            
+        },
+        deleteItem (itemIndex, var1) {
+            console.log(var1);
             this.todos.splice(itemIndex, 1);
+        },
+        doneItem (itemIndex, doneValue) {
+            console.log('click')
+            if (doneValue === true) {
+                this.todos[itemIndex].done = false;
+            } else if (doneValue === false) {
+                this.todos[itemIndex].done = true;
+            }
         }
     },
     mounted() {
 
     },
-}).mount('#app')
+}).mount('#app');
